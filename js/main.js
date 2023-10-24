37fa810ffba2609f2e02aef5514c18d127cf803e
Yura Serkiz <serkiz1040@gmail.com>
2023-10-25 00:12:47 +0300

    burger

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-const menuBtn = document.querySelector(".menu__btn"),
-	menuMobile = document.querySelector(".header__menu-list"),
-	body = document.querySelector("body");
+// const menuBtn = document.querySelector(".menu__btn"),
+// 	menuMobile = document.querySelector(".header__menu-list"),
+// 	body = document.querySelector("body");
 
-menuBtn.addEventListener("click", (e) => {
-	menuMobile.classList.toggle("menu-open");
-});
+// menuBtn.addEventListener("click", (e) => {
+// 	menuMobile.classList.toggle("menu-open");
+// });
+
+"use strict";
+
+const iconMenu = document.querySelector(".menu__icon");
+if (iconMenu) {
+	const menuBody = document.querySelector(".menu__body");
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle("_lock");
+		iconMenu.classList.toggle("_active");
+		menuBody.classList.toggle("_active");
+	});
+}
