44ebcea65a43e9f319181e6dbe5f0bc99f4d9d54
Yura Serkiz <serkiz1040@gmail.com>
2023-10-28 01:30:15 +0300

    course-done

--- js/main.js
+++ js/main.js
@@ -1,19 +1,41 @@
-// const menuBtn = document.querySelector(".menu__btn"),
-// 	menuMobile = document.querySelector(".header__menu-list"),
-// 	body = document.querySelector("body");
-
-// menuBtn.addEventListener("click", (e) => {
-// 	menuMobile.classList.toggle("menu-open");
-// });
-
 "use strict";
 
+// Зміні
 const iconMenu = document.querySelector(".menu__icon");
+const header = document.querySelector(".header");
+const headerPhone = document.querySelector(".header__phone");
+
+// Функціонал
+
 if (iconMenu) {
 	const menuBody = document.querySelector(".menu__body");
 	iconMenu.addEventListener("click", function (e) {
 		document.body.classList.toggle("_lock");
 		iconMenu.classList.toggle("_active");
 		menuBody.classList.toggle("_active");
+		if (iconMenu.classList.contains("_active")) {
+			headerPhone.classList.add("color__black");
+		} else {
+			headerPhone.classList.remove("color__black");
+		}
 	});
 }
+
+const initialBackgroundColor = "#fff8f3";
+let lastScrollPosition = window.scrollY;
+let isAtTop = true;
+
+window.addEventListener("scroll", function () {
+	const currentScrollPosition = window.scrollY;
+
+	if (currentScrollPosition > 0) {
+		isAtTop = false;
+		header.classList.add("active--scroll");
+		// header.style.backgroundColor = "#fff"; // Змінюємо фон при прокрутці вниз
+	} else if (!isAtTop) {
+		isAtTop = true;
+		header.classList.remove("active--scroll");
+
+		// header.style.backgroundColor = initialBackgroundColor; // Повертаємо початковий колір, коли докотилися до верху
+	}
+});
